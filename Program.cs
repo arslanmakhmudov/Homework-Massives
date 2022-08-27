7d18746701bcf23caf759d7626209f0f1552ab55
Arslan <anderfell2003@mail.ru>
2022-08-27 23:15:09 +0500

    Main commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 10
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("Вторая цифра этого числа = " + stringNumber[1]);
+
+// Задача 13
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string numberText = Convert.ToString(number);
+// if (numberText.Length > 2)
+// {
+//     Console.WriteLine("Третья цифра этого числа = " + numberText[2]);
+// }
+// else 
+// {
+//     Console.WriteLine("Третьего числа нет.");
+// }
+
+
+// Задача 15
+
+// Console.WriteLine("Введите число дня недели (Неделя состоит из 7 дней): ");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (dayNumber == 6 || dayNumber == 7)
+// {
+//         Console.WriteLine("Это выходной");
+// }
+// else
+// {
+//         Console.WriteLine("Это Будни");
+// }
